6f341ff72fdca2d9cd1b10615d95a5eeb81b6d22
Taiyo <taiyoperson@gmail.com>
2025-01-26 08:23:59 +1300

    feat: add allFilesTaken() and zero index

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const gameboard = (function() {
     let tiles = ["", "", "", "", "", "", "", "", ""];
 
     const winPatterns = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9],
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
         [1, 4, 7],
         [2, 5, 8],
-        [3, 6, 9],
-        [1, 5, 9],
-        [3, 5, 7]
+        [0, 4, 8],
+        [2, 4, 6]
     ]
 
     function checkForWinner(mark) {
@@ -26,15 +26,21 @@ const gameboard = (function() {
     }
 
     function tileIsEmpty(target) {
-        return tiles[target - 1] === "";
+        return tiles[target] === "";
+    }
+
+    function allTilesTaken() {
+        for (let i = 0; i < tiles.length; i++) {
+            tileIsEmpty(i);
+        }
     }
 
     function tileTakenBy(target, mark) {
-        return tiles[target - 1] === mark;
+        return tiles[target] === mark;
     }
 
     function placeTile(target, mark) {
-        tiles[target - 1] = mark;
+        tiles[target] = mark;
     }
 
     return { tiles, checkForWinner, placeTile };
